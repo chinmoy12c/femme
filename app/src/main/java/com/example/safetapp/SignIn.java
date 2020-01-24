5d70ab6782db7a53addb03eb6f44865b4cbc3756
sanchi0204 <sanchirastogi02@gmail.com>
2020-01-24 23:06:17 +0530

    ui changes

--- app/src/main/java/com/example/safetapp/SignIn.java
+++ app/src/main/java/com/example/safetapp/SignIn.java
@@ -5,12 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.google.android.material.button.MaterialButton;
 
 public class SignIn extends AppCompatActivity {
 
     MaterialButton SignIn;
+    TextView SignUp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +24,15 @@ public class SignIn extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(com.example.safetapp.SignIn.this, Home.class);
+                startActivity(i);
+            }
+        });
+
+        SignUp = findViewById(R.id.signUp_text2);
+        SignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(com.example.safetapp.SignIn.this, Choose_user_type.class);
                 startActivity(i);
             }
         });
