269f14ea7a83fe5bb46c6fe7b3dd0ca8878859e3
Chinmoy Chakraborty <chinmoy12c@gmail.com>
2020-01-25 16:17:41 +0530

    fixes and call feature

--- app/src/main/java/com/example/safetapp/Constants.java
+++ app/src/main/java/com/example/safetapp/Constants.java
@@ -2,6 +2,7 @@ package com.example.safetapp;
 
 public class Constants {
     public static final int LOCATION_UPDATE_DURATION = 30000;
+    public static final String STRESS_CALL_NUMBER = "9315320663";
     //To keep all constants at one place
     static final String PRIMARY_CHANNEL_ID = "FEMME CHANNEL";
     static final CharSequence CHANNEL_NAME = "FEMME SERVICE";
