6a5596d082d6ad35aa772b615020c8d071d450c2
Sanchi Rastogi <46872971+sanchi0204@users.noreply.github.com>
2020-01-25 12:59:26 +0530

    Revert "modified: Constants.java 		//added additional constants"

--- app/src/main/java/com/example/safetapp/StressTracerMap.java
+++ app/src/main/java/com/example/safetapp/StressTracerMap.java
@@ -1,11 +1,9 @@
 package com.example.safetapp;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -13,16 +11,12 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.EventListener;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
 
 public class StressTracerMap extends AppCompatActivity implements OnMapReadyCallback {
 
     GoogleMap googleMap;
     LatLng senderBounds;
-    String senderUsername,userDocumentStress;
+    String senderUsername;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +31,6 @@ public class StressTracerMap extends AppCompatActivity implements OnMapReadyCall
         Intent intent = this.getIntent();
         senderBounds = new LatLng(intent.getDoubleExtra("latitude",45.5),intent.getDoubleExtra("longitude",56.3));
         senderUsername = intent.getStringExtra("username");
-        userDocumentStress = intent.getStringExtra("userDocumentStress");
     }
 
     @Override
@@ -48,26 +41,9 @@ public class StressTracerMap extends AppCompatActivity implements OnMapReadyCall
         googleMap.getUiSettings().setMyLocationButtonEnabled(true);
 
         updateCamera(senderBounds);
-        setUpdateListener();
-    }
-
-    private void setUpdateListener() {
-        FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
-        firebaseFirestore.collection("updatedCoords").document(userDocumentStress).addSnapshotListener(new EventListener<DocumentSnapshot>() {
-            @Override
-            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
-                if (documentSnapshot != null && documentSnapshot.exists()){
-                    Log.d("Updated","got updated data");
-                    double latitude  = documentSnapshot.getDouble("latitude");
-                    double longitude = documentSnapshot.getDouble("longitude");
-                    updateCamera(new LatLng(latitude,longitude));
-                }
-            }
-        });
     }
 
     private void updateCamera(LatLng senderBounds) {
-        googleMap.clear();
         googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(senderBounds,14f));
         googleMap.addMarker(new MarkerOptions().position(senderBounds).title(senderUsername+" is in danger!"));                //TODO:change marker icon
     }
