40ff464d82e7305a54592e8fad3ce9e07b224bc2
sanchi0204 <sanchirastogi02@gmail.com>
2020-01-25 17:43:58 +0530

    ui

--- app/src/main/java/com/example/safetapp/Constants.java
+++ app/src/main/java/com/example/safetapp/Constants.java
@@ -1,6 +1,11 @@
 package com.example.safetapp;
 
 public class Constants {
+<<<<<<< Updated upstream
+=======
+    public static final int LOCATION_UPDATE_DURATION = 30000;
+    public static final String STRESS_CALL_NUMBER = "6394869325";
+>>>>>>> Stashed changes
     //To keep all constants at one place
     static final String PRIMARY_CHANNEL_ID = "FEMME CHANNEL";
     static final CharSequence CHANNEL_NAME = "FEMME SERVICE";
